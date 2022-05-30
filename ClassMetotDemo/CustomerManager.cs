65f6090a1949ec8d72c5e4e995d316d39f8fb1e9
tahiraltundag <tahiraltundag1@gmail.com>
2022-05-30 19:10:21 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+
+        List<Customer> customerList = new List<Customer>();
+
+        public void Add(params Customer[] customers)
+        {
+            foreach(Customer customer in customers)
+            {
+                customerList.Add(customer);
+                Console.WriteLine(customer.FirstName + " Kullanıcısı sisteme kaydedildi\n");
+            }
+        }
+
+        public void Delete(params Customer[] customers)
+        {
+            foreach(Customer customer in customers)
+            {
+                if (customerList.Contains(customer)==true)
+                {
+                    customerList.Remove(customer);
+                    Console.WriteLine(customer.FirstName + " Kullanıcısı sistemden silindi\n");
+                }
+                else
+                {
+                    Console.WriteLine("Müşteri bulunamadı\n");
+                }
+            }
+        }
+
+        public void List()
+        {
+            if(customerList.Count != 0)
+            {
+                Console.WriteLine("Müşeriler listeleniyor\n");
+                foreach(Customer customer in customerList)
+                {
+                    Console.WriteLine(customer.FirstName);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Müşteri listesi boş\n");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customer customer1 = new Customer(1,"Sultan","Altundağ");
+
+            Customer customer2 = new Customer(2,"Mehmet","Altundağ");
+
+            Customer customer3 = new Customer(3,"Hatice","Altundağ");
+
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(customer1, customer2, customer3);
+            customerManager.Delete(customer3);
+            customerManager.List();
+
+        }
+    }
+}
